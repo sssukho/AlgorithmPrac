07fb098345de5fdf8ed79227329afd10ec07abfb
sssukho <sssukho@gmail.com>
2018-10-04 14:19:50 +0900

    init commit

---
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AlgorithmPractice
+//
+//  Created by Sukho Lim on 04/10/2018.
+//  Copyright © 2018 Sukho Lim. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
