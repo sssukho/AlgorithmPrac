f5e91849597d45287bb62d3761633c7a6b8fe00a
sssukho <sssukho@gmail.com>
2018-12-24 13:16:22 +0900

    프로그래머스 43104번 타일장식물

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,83 +1,46 @@
-//DFS, BFS 문제
-//https://programmers.co.kr/learn/courses/30/lessons/43165
+//DP
+//https://programmers.co.kr/learn/courses/30/lessons/43104
 
 /*
- n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+ 그림에서 타일에 적힌 수는 각 타일의 한 변의 길이를 나타낸다. 타일 장식물을 구성하는 정사각형 타일 한 변의 길이를 안쪽 타일부터 시작하여 차례로 적으면 다음과 같다.
+ [1, 1, 2, 3, 5, 8, .]
+ 지수는 문득 이러한 타일들로 구성되는 큰 직사각형의 둘레가 궁금해졌다. 예를 들어, 처음 다섯 개의 타일이 구성하는 직사각형(위에서 빨간색으로 표시한 직사각형)의 둘레는 26이다.
  
- -1+1+1+1+1 = 3
- +1-1+1+1+1 = 3
- +1+1-1+1+1 = 3
- +1+1+1-1+1 = 3
- +1+1+1+1-1 = 3
+ 타일의 개수 N이 주어질 때, N개의 타일로 구성된 직사각형의 둘레를 return 하도록 solution 함수를 작성하시오.
  
- 사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
- 
- 제한사항
- 주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
- 각 숫자는 1 이상 50 이하인 자연수입니다.
- 타겟 넘버는 1 이상 1000 이하인 자연수입니다.
- 
- 입출력 예
- numbers    target    return
- [1, 1, 1, 1, 1]    3    5
- 
- 입출력 예 설명
- 문제에 나온 예와 같습니다.
- 
+ N은 1 이상 80 이하의 자연수
  */
 
 #include <iostream>
-#include <string>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int total;
+int cache[81];
 
-//DFS에서는 어떤 조건을 줘서 종료를 시켜야하는지 생각해야함.
-//어떤 인자로 넘겨주는게 필요한지도 생각.
-
-void DFS(vector<int> &numbers, int &target, int sum, int n) { //n은 깊이
-    if(n >= numbers.size()) {
-        if(sum == target)
-            total = total + 1;
-        return;
+long long solution(int N) {
+    cache[1] = 4;
+    cache[2] = 6;
+    
+    if(N == 1) return cache[N];
+    if(N == 2) return cache[N];
+    
+    for(int i = 3; i < N+1; i++) {
+        cache[i] = cache[i-2] + cache[i-1];
     }
     
-    DFS(numbers, target, sum + numbers[n], n+1); //그래프를 그려보면 됨.
-    DFS(numbers, target, sum - numbers[n], n+1);
+    return cache[N];
 }
-
-int solution(vector<int> numbers, int target) {
-    int answer = 0;
-
-    //제한사항1. 주어지는 숫자들의 개수
-    if(numbers.size() < 2 || numbers.size() > 20)
-        return -1;
-
-    //제한사항2. 타겟넘버 범위
-    if(target < 1 || target > 1000)
-        return -1;
-
-    //제한사항3. 주어지는 숫자들의 범위
-    for(auto x : numbers) {
-        if(x < 1 || x > 50)
-            return -1;
-    }
-    
-    DFS(numbers, target, numbers[0], 1);
-    DFS(numbers, target, -numbers[0], 1);
-    
-    answer = total;
-
-    return answer;
-}
-
 
 int main() {
-    vector<int> vec(5,1);
-    int target = 3;
-
-    cout << solution(vec, target) << endl;
+    
+    cout << solution(5) << endl;
+    cout << "-------------------------" << endl;
+    cout << solution(6) << endl;
+    
+    return 0;
 }
+
+//long long 은 일반 int 형보다 더 큰 범위의 숫자를 다룰 수 있음.
