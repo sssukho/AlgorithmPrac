87673ad389d0852b028b9e2ef75ef73b78bcaa73
sssukho <sssukho@gmail.com>
2019-01-04 18:49:09 +0900

    프로그래머스 42884번 Greedy 단속카메라

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,61 +1,59 @@
+// Greedy(탐욕법)
+// https://programmers.co.kr/learn/courses/30/lessons/42884
+
+/*
+ 고속도로를 이동하는 모든 차량이 고속도로를 이용하면서 단속용 카메라를 한 번은 만나도록 카메라를 설치하려고 합니다.
+ 
+ 고속도로를 이동하는 차량의 경로 routes가 매개변수로 주어질 때, 모든 차량이 한 번은 단속용 카메라를 만나도록 하려면 최소 몇 대의 카메라를 설치해야 하는지를 return 하도록 solution 함수를 완성하세요.
+ 
+ [제한사항]
+ 차량의 대수는 1대 이상 10,000대 이하입니다.
+ routes에는 차량의 이동 경로가 포함되어 있으며 routes[i][0]에는 i번째 차량이 고속도로에 진입한 지점, routes[i][1]에는 i번째 차량이 고속도로에서 나간 지점이 적혀 있습니다.
+ 차량의 진입/진출 지점에 카메라가 설치되어 있어도 카메라를 만난것으로 간주합니다.
+ 차량의 진입 지점, 진출 지점은 -30,000 이상 30,000 이하입니다.
+ 
+ input : [[-20,15], [-14,-5], [-18,-13], [-5,-3]]  /  return : 2
+ 
+ -5 지점에 카메라를 설치하면 두 번째, 네 번째 차량이 카메라를 만납니다.
+ 
+ -15 지점에 카메라를 설치하면 첫 번째, 세 번째 차량이 카메라를 만납니다.
+ */
+
 #include <iostream>
-#include <cstring>
-#include <queue>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-#define MAX 1001
+#define MAX 100
 
-int N, M, V; // 정점의 개수 N, 간선의 개수 M, 탐색을 시작할 정점의 번호 V
-int u, v; //u는 시작 정점, v는 도착 정점.
+bool cmp(vector<int> a, vector<int> b) { return a[1] < b[1]; }
 
-int edge[MAX][MAX];
-bool visited[MAX];
-
-void dfs(int cur) {
-    visited[cur] = true;
-    
-    cout << cur << " ";
-    
-    for(int i = 1; i <= N; i++) {
-        if(edge[cur][i] == 1 && !visited[i])
-            dfs(i);
-    }
-}
-
-void bfs(int cur) {
-    queue<int> q;
-    visited[cur] = true;
-    q.push(cur);
-    
-    while(!q.empty()) {
-        int currentNode = q.front();
-        q.pop();
-        cout << currentNode << " ";
-        for(int i = 1; i <= N; i++) {
-            if(edge[currentNode][i] == 1 && !visited[i]) {
-                visited[i] = true;
-                q.push(i);
-            }
+int solution(vector<vector<int>> routes) {
+    int answer = 0;
+    int limit = -30001;
+    sort(routes.begin(), routes.end(), cmp);
+    for(int i = 0; i < routes.size(); i++){
+        if(limit < routes[i][0]){
+            answer++;
+            limit = routes[i][1];
         }
     }
+    return answer;
 }
 
 int main() {
-    cin.tie(NULL);
-    ios::sync_with_stdio("False");
-    
-    cin >> N >> M >> V;
-    
-    for(int i = 0; i < M; i++) {
-        cin >> u >> v;
-        edge[u][v] = 1;
-        edge[v][u] = 1;
-    }
-    
-    dfs(V);
-    memset(visited, false, sizeof(visited));
-    cout << "\n";
-    bfs(V);
-    
+    vector<vector<int> > routes;
+    routes.resize(MAX);
+
+    routes[0].push_back(-20);
+    routes[0].push_back(15);
+    routes[1].push_back(-14);
+    routes[1].push_back(-5);
+    routes[2].push_back(-18);
+    routes[2].push_back(-13);
+    routes[3].push_back(-5);
+    routes[3].push_back(-3);
+
+    cout << solution(routes) << endl;
 }
