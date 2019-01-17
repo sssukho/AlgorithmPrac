41d66bcaad916616d34b5a15afee9a6c20c57b4a
sssukho <sssukho@gmail.com>
2019-01-17 15:44:35 +0900

    백준 9205번 맥주 마시면서 걸어가기 혜리님 코드가 제대로 성공함. 이전꺼는 무시.

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,4 +1,11 @@
 /*
+ 송도에 사는 상근이와 친구들은 송도에서 열리는 펜타포트 락 페스티벌에 가려고 한다. 올해는 맥주를 마시면서 걸어가기로 했다. 출발은 상근이네 집에서 하고, 맥주 한 박스를 들고 출발한다. 맥주 한 박스에는 맥주가 20개 들어있다. 목이 마르면 안되기 때문에 50미터에 한 병씩 마시려고 한다.
+ 
+ 상근이의 집에서 페스티벌이 열리는 곳은 매우 먼 거리이다. 따라서, 맥주를 더 구매해야 할 수도 있다. 미리 인터넷으로 조사를 해보니 다행히도 맥주를 파는 편의점이 있다. 편의점에 들렸을 때, 빈 병은 버리고 새 맥주 병을 살 수 있다. 하지만, 박스에 들어있는 맥주는 20병을 넘을 수 없다.
+ 
+ 편의점, 상근이네 집, 펜타포트 락 페스티벌의 좌표가 주어진다. 상근이와 친구들이 행복하게 페스티벌에 도착할 수 있는지 구하는 프로그램을 작성하시오.
+ 
+ [입력]
  첫째 줄에 테스트 케이스의 개수 t가 주어진다. (t ≤ 50)
  
  각 테스트 케이스의 첫째 줄에는 맥주를 파는 편의점의 개수 n이 주어진다. (0 ≤ n ≤ 100).
@@ -7,214 +14,76 @@
  
  송도는 직사각형 모양으로 생긴 도시이다. 두 좌표 사이의 거리는 x 좌표의 차이 + y 좌표의 차이 이다. (맨해튼 거리)
  
+ [출력]
+ 각 테스트 케이스에 대해서 상근이와 친구들이 행복하게 페스티벌에 갈 수 있으면 "happy", 중간에 맥주가 바닥나면 "sad"를 출력한다.
  */
 
-//50미터에 한 병씩 먹고
-//편의점에 들렀을 때 빈병은 버리고 새 맥주 병을 살 수 있다.
-//박스에 들어있는 맥주는 20병을 넘을 수 없다.
-/*
 #include <iostream>
-#include <queue>
 #include <algorithm>
-#include <cstring>
+#include <queue>
 
 using namespace std;
 
-#define MAX 70000
-
-int T; // 테스트 케이스 수
-int N; // 매궂를 파는 편의점의 개수
-int sy, sx; // 상근이네 집
-int py, px; // 편의점
-int fy, fx; // 락페
-
-int edge[MAX][MAX];
-bool visited[MAX][MAX];
-int dist[MAX][MAX];
-
-int dy[4] = {1, -1, 0, 0};
-int dx[4] = {0, 0, 1, -1};
-
-bool isHappy = false;
-int beerBox = 20;
-
-int ySize = 0, xSize = 0;
-
-void dfs(int y, int x) {
-    
-    if(y == fy && x == fx) {
-        if(beerBox >= 0)
-            isHappy = true;
-        return;
-    }
-    
-    visited[y][x] = true;
-    
-    for(int i = 0; i < 4; i++) {
-        int my = y + dy[i];
-        int mx = x + dx[i];
-        
-        if(my >= sy && my < fy && mx >= sx && mx < fx) {
-            //그냥 갈 수 있는 길이면
-            if(!visited[my][mx] && edge[my][mx] == 0) {
-                dist[my][mx] = (mx-x) + (my-y);
-                if(dist[my][mx] >= 50) {
-                    dist[my][mx] = 0;
-                    beerBox--;
-                }
-                visited[my][mx] = true;
-                dfs(my, mx);
-            }
-            //편의점인 경우
-            else if(!visited[my][mx] && edge[my][mx] == 1) {
-                visited[my][mx] = true;
-                beerBox = 20;
-                dfs(my, mx);
-            }
-        }
-        
-        if(beerBox <= 0) {
-            isHappy = false;
-            return;
-        }
-    }
-}
+int T, n, sx, sy ,ex ,ey, store[100][2], visited[101];
+bool check;
 
 int main() {
     cout << "start" << endl;
     cin.tie(NULL);
     ios::sync_with_stdio("False");
     
-    cin >> T;
-    for(int i = 0; i < T; i++) {
-        memset(edge, 0, sizeof(edge));
-        memset(visited, 0, sizeof(visited));
-        cin >> N;
-        cin >> sx >> sy;
-        edge[sy][sx] = 2; //상근이의 현재 위치
-        for(int j = 0; j < N; j++) {
-            cin >> px >> py;
-            edge[py][px] = 1; //편의점
-        }
-        cin >> fx >> fy;
-        edge[fy][fx] = 3;//페스티벌 위치
-    }
-    
-    ySize = fy-sy;
-    xSize = fx-sx;
-    
-    for(int i = sy; i < fy; i++) {
-        for(int j = sx; j < fx; j++) {
-            if((!visited[i][j] && edge[i][j] == 1) || edge[i][j] == 2) {
-                dfs(i, j);
-                beerBox = 20;
-            }
-        }
-    }
-    
-    if(isHappy)
-        cout << "happy" << endl;
-    else
-        cout << "sad" << endl;
-    
-    return 0;
-}
-*/
-
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring> //memset
-
-using namespace std;
-
-const int MAX = 100 + 2; //출발점, 도착점 포함
-
-int N;
-vector<int> graph[MAX];
-bool visited[MAX];
-
-//맨해튼 거리
-int distance(pair<int, int> p1, pair<int, int> p2){
-    return abs(p1.first - p2.first) + abs(p1.second - p2.second);
-}
-
-//전형적인 DFS
-void DFS(int node){
-    visited[node] = true;
-    for (int i = 0; i < graph[node].size(); i++)
-    {
-        int next = graph[node][i];
-        if (visited[next] == false)
-            DFS(next);
-    }
-}
-
-int main(void){
-    cout << "start" << endl;
-    ios_base::sync_with_stdio(0);
-    cin.tie(0); //cin 속도 향상
-    
-    int test_case;
-    cin >> test_case;
-    
-    for (int i = 0; i < test_case; i++)
-    {
-        for (int j = 0; j < MAX; j++)
-            graph[j].clear();
-        memset(visited, false, sizeof(visited));
+    cin >> T; //테스트케이스 수
+    for(int t = 0; t < T; t++) {
+        cin >> n; //편의점 수
+        cin >> sx >> sy; //출발점 좌표(상근이네 집)
         
-        cin >> N;
-        
-        vector<pair<int, int>> coord;
-        
-        
-        
-        //출발점(0), 도착점(N + 1) 포함
-        
-        for (int j = 0; j < N + 2; j++)
-            
-        {
-            
-            int y, x;
-            
-            cin >> y >> x;
-            
-            
-            
-            coord.push_back(make_pair(y, x));
-            
+        for(int i = 0; i < n; i++) {
+            cin >> store[i][0] >> store[i][1];
+            visited[i] = false;
         }
         
+        cin >> ex >> ey; //락페 좌표
+        int r = abs(sx-ex) + abs(sy-ey); //(현재 y좌표 - 락페 y좌표) + 현재 x좌표 - 락페 x좌표)의 절대값끼리 더한것
+        check = false; //종료 조건 줄라고
         
-        
-        //맥주 20병으로 갈 수 있는 거리 내에 있으면 그래프에 추가
-        
-        for(int j=0; j<N+2; j++)
-            
-            for(int k=j+1; k<N+2; k++)
-                
-                if (distance(coord[j], coord[k]) <= 50 * 20)
+        if(r <= 1000)
+            check = true;
+        else {
+            queue<pair<int, int>> q;
+            q.push(make_pair(sx, sy));
+            while(!q.empty()) {
+                int qn = q.size();
+                for(int k = 0; k < qn; k++) {
+                    int nx = q.front().first;
+                    int ny = q.front().second;
+                    q.pop();
                     
-                {
                     
-                    //양방향 그래프
-                    
-                    graph[j].push_back(k);
-                    
-                    graph[k].push_back(j);
-                    
+                    if(abs(nx - ex) + abs(ny -ey) <= 1000) {
+                        check = true;
+                        while(!q.empty())
+                            q.pop();
+                        break;
+                    }
+                    for(int i = 0; i < n; i++) {
+                        //집과 펜타포트 위치 사이 안에 있지 않고 밖에 있는 편의점도 가능하게 했더니 pass
+                        //원래 abs(mx-nx) + abs(my-ny) <= r로 설정했었는데 fail
+                        int mx = store[i][0], my = store[i][1];
+                        if(!visited[i] && abs(mx-nx) + abs(my-ny) <= 1000) {
+                            q.push(make_pair(mx, my));
+                            visited[i] = true;
+                        }
+                    }
                 }
-        DFS(0);
-        
-        //0이 출발점, N+1이 도착점이므로
-        if (visited[N + 1])
+            }
+            while(!q.empty())
+                q.pop();
+        }
+        if(check)
             cout << "happy\n";
         else
             cout << "sad\n";
-        
     }
     
     return 0;
-    
 }
