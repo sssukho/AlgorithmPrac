274aad99bb1557e55df556996fc12fbb1db60559
sssukho <sssukho@gmail.com>
2019-01-19 16:09:30 +0900

    백준 1405번 미친 로봇(백트레킹)

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,68 +1,71 @@
-//https://programmers.co.kr/learn/courses/30/lessons/12977
-//프로그래머스 2017 서머코딩 소수 만들기
+/*
+ 문제
+ 통제 할 수 없는 미친 로봇이 평면위에 있다. 그리고 이 로봇은 N번의 행동을 취할 것이다.
+ 
+ 각 행동에서 로봇은 4개의 방향 중에 하나를 임의로 선택한다. 그리고 그 방향으로 한 칸 이동한다.
+ 
+ 로봇이 같은 곳을 한 번보다 많이 이동하지 않을 때, 로봇의 이동 경로가 단순하다고 한다. (로봇이 시작하는 위치가 처음 방문한 곳이다.) 로봇의 이동 경로가 단순할 확률을 구하는 프로그램을 작성하시오. 예를 들어, EENE와 ENW는 단순하지만, ENWS와 WWWWSNE는 단순하지 않다. (E는 동, W는 서, N은 북, S는 남)
+ 
+ 입력
+ 첫째 줄에 N, 동쪽으로 이동할 확률, 서쪽으로 이동할 확률, 남쪽으로 이동할 확률, 북쪽으로 이동할 확률이 주어진다. N은 14보다 작거나 같은 자연수이고,  모든 확률은 100보다 작거나 같은 자연수 또는 0이다. 그리고, 동서남북으로 이동할 확률을 모두 더하면 100이다.
+ 
+ 출력
+ 첫재 줄에 로봇의 이동 경로가 단순할 확률을 출력한다. 절대/상대 오차는 10-9 까지 허용한다.
+ */
 
-#include <vector>
 #include <iostream>
-#include <cmath>
 #include <algorithm>
 #include <cstring>
 
 using namespace std;
+#define MAX 31
 
-#define MAX 3000
-bool isPrime[MAX];
+int N; //입력받는 것들
+double percent[4]; //percentage
 
-void eratosthenes() {
-    for(int i = 2; i <= MAX; i++){
-        isPrime[i] = true;
+int dy[4] = {1, -1, 0, 0};
+int dx[4] = {0 , 0, 1, -1};
+
+bool visited[MAX][MAX];
+double result = 0;
+
+void dfs(int y, int x, int cnt, double prob) {
+    if(cnt == N) {
+        result = result + prob;
+        return;
     }
     
-    int sqrtn = sqrt(MAX);
-    
-    for(int i = 2; i <= sqrtn; i++) {
-        if(isPrime[i]){
-            for(int j = i + i; j <= MAX; j=j+i) {
-                isPrime[j] = false;
+    for(int i = 0; i < 4; i++) {
+        int my = y + dy[i];
+        int mx = x + dx[i];
+        
+        if(my >= 0 && my < MAX && mx >= 0 && mx < MAX) {
+            if(!visited[my][mx]) {
+                visited[my][mx] = true;
+                dfs(my, mx, cnt+1, prob * percent[i]);
+                visited[my][mx] = false;
             }
         }
     }
-}
-
-int solution(vector<int> nums) {
-    int answer = 0;
-    
-    eratosthenes();
-    
-    for(int i = 0; i < nums.size(); i++) {
-        for(int j = i+1; j < nums.size(); j++) {
-            for(int k = j+1; k < nums.size(); k++) {
-                int sum = nums[i] + nums[j] + nums[k];
-                if(isPrime[sum])
-                    answer++;
-            }
-        }
-    }
-    
-    return answer;
 }
 
 int main() {
-    vector<int> nums1;
-    vector<int> nums2;
+    cout << "start" << endl;
+    cin.tie(NULL);
+    ios::sync_with_stdio("False");
     
-    nums1.push_back(1);
-    nums1.push_back(2);
-    nums1.push_back(3);
-    nums1.push_back(4);
+    cin >> N;
     
-    nums2.push_back(1);
-    nums2.push_back(2);
-    nums2.push_back(7);
-    nums2.push_back(6);
-    nums2.push_back(4);
+    for(int i = 0; i < 4; i++) {
+        cin >> percent[i];
+        percent[i] = (double)percent[i] / 100;
+    }
     
-    cout << solution(nums1) << endl;
-    cout << solution(nums2) << endl;
+    visited[14][14] = true;
+    dfs(14, 14, 0, 1);
+    
+    cout.precision(9);
+    cout << result << endl;
     
     return 0;
 }
