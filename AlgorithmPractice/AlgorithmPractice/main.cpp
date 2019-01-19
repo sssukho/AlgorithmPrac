7daa00c9d9381227eda4d5e33c930c71e3f5f7d2
sssukho <sssukho@gmail.com>
2019-01-19 18:36:05 +0900

    프로그래머스 징검다리 이분탐색

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,71 +1,76 @@
 /*
- 문제
- 통제 할 수 없는 미친 로봇이 평면위에 있다. 그리고 이 로봇은 N번의 행동을 취할 것이다.
+ <문제 설명>
+ 출발지점부터 distance만큼 떨어진 곳에 도착지점이 있습니다. 그리고 그사이에는 바위들이 놓여있습니다. 바위 중 몇 개를 제거하려고 합니다.
+ 예를 들어, 도착지점이 25만큼 떨어져 있고, 바위가 [2, 14, 11, 21, 17] 지점에 놓여있을 때 바위 2개를 제거하면 출발지점, 도착지점, 바위 간의 거리가 아래와 같습니다.
  
- 각 행동에서 로봇은 4개의 방향 중에 하나를 임의로 선택한다. 그리고 그 방향으로 한 칸 이동한다.
+ 제거한 바위의 위치    각 바위 사이의 거리    거리의 최솟값
+ [21, 17]         [2, 9, 3, 11]      2
+ [2, 21]          [11, 3, 3, 8]      3
+ [2, 11]          [14, 3, 4, 4]      3
+ [11, 21]         [2, 12, 3, 8]      2
+ [2, 14]          [11, 6, 4, 4]      4
+ 위에서 구한 거리의 최솟값 중에 가장 큰 값은 4입니다.
  
- 로봇이 같은 곳을 한 번보다 많이 이동하지 않을 때, 로봇의 이동 경로가 단순하다고 한다. (로봇이 시작하는 위치가 처음 방문한 곳이다.) 로봇의 이동 경로가 단순할 확률을 구하는 프로그램을 작성하시오. 예를 들어, EENE와 ENW는 단순하지만, ENWS와 WWWWSNE는 단순하지 않다. (E는 동, W는 서, N은 북, S는 남)
+ 출발지점부터 도착지점까지의 거리 distance, 바위들이 있는 위치를 담은 배열 rocks, 제거할 바위의 수 n이 매개변수로 주어질 때, 바위를 n개 제거한 뒤 각 지점 사이의 거리의 최솟값 중에 가장 큰 값을 return 하도록 solution 함수를 작성해주세요.
  
- 입력
- 첫째 줄에 N, 동쪽으로 이동할 확률, 서쪽으로 이동할 확률, 남쪽으로 이동할 확률, 북쪽으로 이동할 확률이 주어진다. N은 14보다 작거나 같은 자연수이고,  모든 확률은 100보다 작거나 같은 자연수 또는 0이다. 그리고, 동서남북으로 이동할 확률을 모두 더하면 100이다.
+ <제한사항>
+ 도착지점까지의 거리 distance는 1 이상 1,000,000,000 이하입니다.
+ 바위는 1개 이상 50,000개 이하가 있습니다.
+ n 은 1 이상 바위의 개수 이하입니다.
  
- 출력
- 첫재 줄에 로봇의 이동 경로가 단순할 확률을 출력한다. 절대/상대 오차는 10-9 까지 허용한다.
+ <입출력 예>
+ distance    rocks                 n    return
+ 25         [2, 14, 11, 21, 17]    2    4
  */
 
 #include <iostream>
 #include <algorithm>
-#include <cstring>
-
+#include <string>
+#include <vector>
 using namespace std;
-#define MAX 31
 
-int N; //입력받는 것들
-double percent[4]; //percentage
+int solution(int distance, vector<int> rocks, int n) {
+    int answer = 0;
+    
+    sort(rocks.begin(), rocks.end());
+    
+    int low = 0;
+    int high = 1000000001;
 
-int dy[4] = {1, -1, 0, 0};
-int dx[4] = {0 , 0, 1, -1};
-
-bool visited[MAX][MAX];
-double result = 0;
-
-void dfs(int y, int x, int cnt, double prob) {
-    if(cnt == N) {
-        result = result + prob;
-        return;
+    for(int i = 0; i < 100; i++) {
+        int middle = (low+high) / 2;
+        int limit = 0;
+        int remove = 0;
+        
+        for(int j = 0; j < rocks.size(); j++) {
+            if(rocks[j] - limit < middle)
+                remove++;
+            else
+                limit = rocks[j];
+        }
+        
+        if(remove > n)
+            high = middle;
+        else
+            low = middle;
     }
     
-    for(int i = 0; i < 4; i++) {
-        int my = y + dy[i];
-        int mx = x + dx[i];
-        
-        if(my >= 0 && my < MAX && mx >= 0 && mx < MAX) {
-            if(!visited[my][mx]) {
-                visited[my][mx] = true;
-                dfs(my, mx, cnt+1, prob * percent[i]);
-                visited[my][mx] = false;
-            }
-        }
-    }
+    answer = low;
+    return answer;
 }
 
 int main() {
-    cout << "start" << endl;
-    cin.tie(NULL);
-    ios::sync_with_stdio("False");
+    int distance = 25;
+    int n = 2;
     
-    cin >> N;
+    vector<int> rocks;
+    rocks.push_back(2);
+    rocks.push_back(14);
+    rocks.push_back(11);
+    rocks.push_back(21);
+    rocks.push_back(17);
     
-    for(int i = 0; i < 4; i++) {
-        cin >> percent[i];
-        percent[i] = (double)percent[i] / 100;
-    }
-    
-    visited[14][14] = true;
-    dfs(14, 14, 0, 1);
-    
-    cout.precision(9);
-    cout << result << endl;
+    cout << solution(distance, rocks, n) << endl;
     
     return 0;
 }
