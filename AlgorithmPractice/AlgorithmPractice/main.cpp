8d41766a0d191836b8477143ab77ee51103f3bdc
sssukho <sssukho@gmail.com>
2019-01-23 14:31:44 +0900

    백준 11559 puyo puyo

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,98 +1,89 @@
-//# 은 금으로 막혀있어 지나갈 수 없는 칸
-//. 비어있는 칸은
-//시작지점은 'S'로 표현
-//탈출 출구는 'E'
-//각 층 사이에는 빈줄이 있으며, 입력의 끝은 L,R,C가 모두 0으로 표현된다.
 #include <iostream>
-#include <algorithm>
 #include <cstring>
-#include <string>
-#include <queue>
+#include <vector>
 using namespace std;
-#define MAX 31
 
-int L,R,C; //빌딩의 층수, 세로, 가로
-char building[MAX][MAX][MAX]; //층수, 세로, 가로
-bool visited[MAX][MAX][MAX]; //층수, 세로, 가로
-int time_building[MAX][MAX][MAX];
+char map[12][6];
+int num[73] = {0, };
+int visited[12][6];
+int dy[4] = {1, -1, 0, 0};
+int dx[4] = {0, 0, 1, -1};
+int idx = 0;
 
-int dh[6] = {1, -1, 0, 0, 0, 0};
-int dy[6] = {0, 0, 1, -1, 0, 0};
-int dx[6] = {0, 0, 0, 0, 1, -1};
-int currentH;
-int currentY;
-int currentX;
-int destH;
-int destY;
-int destX;
-
-void bfs() {
-    queue<pair<int, pair<int, int>>> q; //층수, y, x
-    q.push(make_pair(currentH, make_pair(currentY, currentX)));
-    
-    while(!q.empty()) {
-        int h = q.front().first;
-        int y = q.front().second.first;
-        int x = q.front().second.second;
-        q.pop();
+void dfs(int y, int x, char ch) {
+    visited[y][x] = idx;
+    num[idx]++;
+    for(int i = 0; i < 4; i++) {
+        int my = y + dy[i];
+        int mx = x + dx[i];
         
-        if(h == destH && y == destY && x == destX) {
-            cout << "Escaped in " << time_building[h][y][x] << " minute(s).\n";
-            return;
+        if(my >= 0 && my < 12 && mx >= 0 && mx < 6) {
+            if(visited[my][mx] == 0 && map[my][mx] != '.' && map[my][mx] == map[y][x])
+                dfs(my, mx, map[y][x]);
         }
-        
-        for(int k = 0; k < 6; k++) {
-            int mh = h + dh[k];
-            int my = y + dy[k];
-            int mx = x + dx[k];
-            
-            if(mh >= 0 && mh < L && my >= 0 && my < R && mx >= 0 && mx < C) {
-                if((!visited[mh][my][mx] && building[mh][my][mx] == '.') || building[mh][my][mx] == 'E') {
-                    visited[mh][my][mx] = true;
-                    q.push(make_pair(mh, make_pair(my, mx)));
-                    time_building[mh][my][mx] = time_building[h][y][x] + 1;
-                }
-            }
-        }
-        
     }
-    
-    cout << "Trapped!\n";
 }
+
 
 int main() {
     cout << "start" << endl;
     cin.tie(NULL);
     ios::sync_with_stdio("False");
     
-    while(true) {
-        memset(building, 0, sizeof(building));
-        memset(visited, false, sizeof(visited));
-        memset(time_building, 0, sizeof(time_building));
+    for(int i = 0; i < 12; i++) {
+        for(int j = 0; j < 6; j++) {
+            cin >> map[i][j];
+        }
+    }
+    
+    int ans = 0;
+    bool puyo = true; //뿌요 1개이상 되는지
+    while(puyo) {
+        memset(visited, 0, sizeof(visited));
+        memset(num, 0, sizeof(num));
+        puyo = false;
+        //탐색 : visit 체크(증가), puyo 체크, room 개수 증가
+        idx = 1;
+        for(int i = 0; i < 12; i++) {
+            for(int j = 0; j < 6; j++) {
+                if(map[i][j] != '.' && visited[i][j] == 0) {
+                    dfs(i, j, map[i][j]);
+                    idx++;
+                }
+            }
+        }
         
-        //cin >> L >> R >> C;
-        scanf("%d %d %d", &L, &R, &C);
-        if(L == 0 && R == 0 && C == 0)
-            return 0;
-        
-        for(int i = 0; i < L; i++) {
-            for(int j = 0; j < R; j++) {
-                for(int k = 0; k < C; k++) {
-                    cin >> building[i][j][k];
-                    if(building[i][j][k] == 'S') {
-                        currentH = i;
-                        currentY = j;
-                        currentX = k;
-                    }
-                    if(building[i][j][k] == 'E') {
-                        destH = i;
-                        destY = j;
-                        destX = k;
+        //num중에 4 이상이 있으면 뿌요 가능 -> map '.' 으로
+        for(int k = 1; k <= idx; k++) {
+            if(num[k] >= 4) {
+                puyo = true;
+                for(int i = 0; i < 12; i++) {
+                    for(int j = 0; j < 6; j++) {
+                        if(visited[i][j] == k)
+                            map[i][j] = '.';
                     }
                 }
             }
         }
         
-        bfs();
+        //한 column씩 밑으로 내려주기
+        for(int j = 0; j < 6; j++) {
+            vector<char> drop;
+            for(int i = 11; i >= 0; i--) { //아래서부터
+                if(map[i][j] != '.')
+                    drop.push_back(map[i][j]);
+            }
+            int cnt = 0;
+            for(int k = 11; k > 11-drop.size(); k--) {
+                map[k][j] = drop[cnt++];
+            }
+            for (int k = 11-drop.size(); k>= 0; k--) {
+                map[k][j] = '.';
+            }
+        }
+        if(puyo) ans++;
+        
     }
+    cout << ans;
+    return 0;
 }
