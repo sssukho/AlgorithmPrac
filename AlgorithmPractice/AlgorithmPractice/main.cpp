ab5bae9e2a7c9295a222ecc662745cec70d49afd
sssukho <sssukho@gmail.com>
2019-01-25 00:43:44 +0900

    백준 30555번 탈출

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,89 +1,114 @@
 #include <iostream>
+#include <queue>
+#include <string>
 #include <cstring>
-#include <vector>
+
 using namespace std;
+#define MAX 50
 
-char map[12][6];
-int num[73] = {0, };
-int visited[12][6];
-int dy[4] = {1, -1, 0, 0};
-int dx[4] = {0, 0, 1, -1};
-int idx = 0;
+int R, C; //세로 가로
+string map[MAX];
+int time_way[MAX][MAX] = { 0, };
 
-void dfs(int y, int x, char ch) {
-    visited[y][x] = idx;
-    num[idx]++;
-    for(int i = 0; i < 4; i++) {
-        int my = y + dy[i];
-        int mx = x + dx[i];
+int dy[4] = { 1, -1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int startY, startX;
+int destY, destX;
+
+queue<pair<int, int>> coord;
+
+void makeWater() {
+    int coordSize = (int)coord.size();
+    
+    for (int i = 0; i < coordSize; i++) {
+        int y = coord.front().first;
+        int x = coord.front().second;
+        coord.pop();
         
-        if(my >= 0 && my < 12 && mx >= 0 && mx < 6) {
-            if(visited[my][mx] == 0 && map[my][mx] != '.' && map[my][mx] == map[y][x])
-                dfs(my, mx, map[y][x]);
+        for (int i = 0; i < 4; i++) {
+            int my = y + dy[i];
+            int mx = x + dx[i];
+            
+            if (my >= 0 && my < R && mx >= 0 && mx < C) {
+                if (map[my][mx] != 'X' && map[my][mx] != 'D' && map[my][mx] != '*') {//S도 될수 있음
+                    map[my][mx] = '*';
+                    coord.push(make_pair(my, mx));
+                }
+            }
         }
     }
 }
 
-
-int main() {
-    cout << "start" << endl;
-    cin.tie(NULL);
-    ios::sync_with_stdio("False");
+bool bfs() {
+    queue<pair<int, int>> q;
+    q.push(make_pair(startY, startX));
     
-    for(int i = 0; i < 12; i++) {
-        for(int j = 0; j < 6; j++) {
-            cin >> map[i][j];
-        }
-    }
+    time_way[startY][startX] = 1;
+    makeWater();
     
-    int ans = 0;
-    bool puyo = true; //뿌요 1개이상 되는지
-    while(puyo) {
-        memset(visited, 0, sizeof(visited));
-        memset(num, 0, sizeof(num));
-        puyo = false;
-        //탐색 : visit 체크(증가), puyo 체크, room 개수 증가
-        idx = 1;
-        for(int i = 0; i < 12; i++) {
-            for(int j = 0; j < 6; j++) {
-                if(map[i][j] != '.' && visited[i][j] == 0) {
-                    dfs(i, j, map[i][j]);
-                    idx++;
-                }
-            }
-        }
+    int qn = (int)q.size();
+    
+    while (!q.empty()) {
+        int y = q.front().first;
+        int x = q.front().second;
+        q.pop();
         
-        //num중에 4 이상이 있으면 뿌요 가능 -> map '.' 으로
-        for(int k = 1; k <= idx; k++) {
-            if(num[k] >= 4) {
-                puyo = true;
-                for(int i = 0; i < 12; i++) {
-                    for(int j = 0; j < 6; j++) {
-                        if(visited[i][j] == k)
-                            map[i][j] = '.';
+        if (y == destY && x == destX)
+            return true;
+        
+        for (int i = 0; i < 4; i++) {
+            int my = y + dy[i];
+            int mx = x + dx[i];
+            
+            if (my >= 0 && my < R && mx >= 0 && mx < C) {
+                if(time_way[my][mx]==0){
+                    if (map[my][mx] == '.' || map[my][mx] == 'D') {
+                        time_way[my][mx] = time_way[y][x] + 1;
+                        q.push(make_pair(my, mx));
                     }
                 }
             }
         }
         
-        //한 column씩 밑으로 내려주기
-        for(int j = 0; j < 6; j++) {
-            vector<char> drop;
-            for(int i = 11; i >= 0; i--) { //아래서부터
-                if(map[i][j] != '.')
-                    drop.push_back(map[i][j]);
+        //1초... 큐마다 도는게 아니라..
+        qn--;
+        if (qn == 0) {
+            qn = (int)q.size();
+            makeWater();
+        }
+    }
+    
+    return false;
+}
+
+int main() {
+    cin.tie(NULL);
+    ios::sync_with_stdio("False");
+    
+    cin >> R >> C;
+    
+    for (int i = 0; i < R; i++) {
+        cin >> map[i];
+        
+        for (int j = 0; j < C; j++) {
+            if (map[i][j] == 'S') {
+                startY = i;
+                startX = j;
             }
-            int cnt = 0;
-            for(int k = 11; k > 11-drop.size(); k--) {
-                map[k][j] = drop[cnt++];
+            if (map[i][j] == 'D') {
+                destY = i;
+                destX = j;
             }
-            for (int k = 11-drop.size(); k>= 0; k--) {
-                map[k][j] = '.';
+            if (map[i][j] == '*') {
+                coord.push(make_pair(i, j));
             }
         }
-        if(puyo) ans++;
-        
     }
-    cout << ans;
+    if (bfs())
+        cout << time_way[destY][destX]-1;
+    else
+        cout << "KAKTUS";
+    
     return 0;
 }
