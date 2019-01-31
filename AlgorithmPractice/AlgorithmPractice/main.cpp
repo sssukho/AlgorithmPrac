5bafa3afa7c6d6006b47a5c08ab94516e4a022b8
sssukho <sssukho@gmail.com>
2019-01-31 13:57:09 +0900

    백준 2174 로봇 시뮬레이션

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,47 +1,153 @@
-/*
- 문제
- N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
- 
- N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
- 
- 입력
- 첫째 줄에 N이 주어진다. (1 ≤ N < 15)
- 
- 출력
- 첫째 줄에 퀸 N개를 서로 공격할 수 없게 놓는 경우의 수를 출력한다.
- */
-
 #include <iostream>
-#include <cstring>
+#include <queue>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int n;
-bool *arr; //열의 개수 (세로 개수)
-bool *d1; //오른쪽 위를 향하는 대각(각 자리의 합) = (2n-1)개
-bool *d2; //오른쪽 아래를 향하는 대각 {(n-1) + (행-열) 로 구분}
-int result = 0;
+int east[2] = {0, 1}; //y,x
+int west[2] = {0, -1};
+int south[2] = {1, 0};
+int north[2] = {-1, 0};
 
-void Solution(int y) {
-    if(y >= n) result++;
-    
-    for(int i = 0; i < n; i++) {
-        if(arr[i]) continue; //arr[i]가 존재하면 continue;
-        if(d1[y+i] || d2[n-1+(y-i)]) continue; //대각선상에 존재하면 continue;
-        
-        arr[i] = d1[y+i] = d2[n-1 + (y-i)] = true; //체크하고
-        Solution(y+1); //아래 행으로 넘어감
-        arr[i] = d1[y+i] = d2[n-1+(y-i)] = false; //백트랙킹 조건
+int A, B; //가로 세로
+int N, M; //로봇 개수, 명령 개수
+
+int map[102][102];
+vector<pair<pair<int, int>, char>> robot_position; //로봇들의 초기 위치(y,x), 상태
+queue<pair<int, pair<char,int>>> order;//로봇 번호, 명령 종류, 해당 명령 반복 횟수
+pair<int, int> direction;
+
+void setDirection(int robotNum, char dir, char left_right) {
+    if(left_right == 'l') {
+        if(dir == 'E') {
+            robot_position[robotNum].second = 'N';
+        }
+        else if(dir == 'W') {
+            robot_position[robotNum].second = 'S';
+        }
+        else if(dir == 'S') {
+            robot_position[robotNum].second = 'E';
+        }
+        else if(dir == 'N') {
+            robot_position[robotNum].second = 'W';
+        }
     }
+    
+    else {
+        if(dir == 'E') {
+            robot_position[robotNum].second = 'S';
+        }
+        else if(dir == 'W') {
+            robot_position[robotNum].second = 'N';
+        }
+        else if(dir == 'S') {
+            robot_position[robotNum].second = 'W';
+        }
+        else if(dir == 'N') {
+            robot_position[robotNum].second = 'E';
+        }
+    }
+    
+}
+
+pair<int, int> moveDirection(int y, int x, char dir) {
+    int my = 0, mx = 0;
+    if(dir == 'E') {
+        my = y + east[0];
+        mx = x + east[1];
+    }
+    else if(dir == 'W') {
+        my = y + west[0];
+        mx = x + west[1];
+    }
+    else if(dir == 'S') {
+        my = y + south[0];
+        mx = x + south[1];
+    }
+    else if(dir == 'N') {
+        my = y + north[0];
+        mx = x + north[1];
+    }
+    return make_pair(my, mx);
+}
+
+void simulation() {
+    while(!order.empty()) {
+        int robotNum = order.front().first;
+        char ord = order.front().second.first;
+        int repeatNum = order.front().second.second;
+        order.pop();
+        
+        for(int i = 0; i < repeatNum; i++) {
+            int y = robot_position[robotNum-1].first.first;
+            int x = robot_position[robotNum-1].first.second;
+            int my = 0;
+            int mx = 0;
+            
+            if(ord == 'F') {
+                cout << "----------------" << endl;
+                for(int i = 0; i < B; i++) {
+                    for(int j = 0; j < A; j++) {
+                        cout << map[i][j];
+                    }
+                    cout << endl;
+                }
+                direction = moveDirection(y, x, robot_position[robotNum-1].second);
+                my = direction.first;
+                mx = direction.second;
+                
+                if(my < 0 || my >= B || mx < 0 || mx >= A) {
+                    cout << "Robot " << robotNum << " crashes into the wall\n";
+                    return;
+                }
+                
+                else if(map[my][mx] != 0) {
+                    cout << "Robot " << robotNum << " crashes into robot " << map[my][mx] << "\n";
+                    return;
+                }
+                
+                else if(my >= 0 && my < B && mx >= 0 && mx < A) {
+                    map[y][x] = 0;
+                    robot_position[robotNum-1].first.first = my;
+                    robot_position[robotNum-1].first.second = mx;
+                    map[my][mx] = robotNum;
+                }
+            }
+            else if(ord == 'L') {
+                setDirection(robotNum-1, robot_position[robotNum-1].second, 'l');
+            }
+            else if(ord == 'R') {
+                setDirection(robotNum-1, robot_position[robotNum-1].second, 'r');
+            }
+        }
+    }
+    cout << "OK" << endl;
 }
 
 int main() {
     cout << "start" << endl;
-    cin >> n;
-    arr = new bool[n];
-    d1 = new bool[2*n-1];
-    d2 = new bool[2*n-1];
+    cin.tie(NULL);
+    ios::sync_with_stdio("False");
     
-    Solution(0);
-    cout << result << endl;
+    cin >> A >> B;
+    cin >> N >> M;
 
+    for(int i = 1; i <= N; i++) {
+        int x, y;
+        char s;
+        cin >> x >> y >> s;
+        robot_position.push_back(make_pair(make_pair(B-y, x-1), s));
+        map[B-y][x-1] = i; //로봇의 초기 위치 지정.
+    }
+
+    for(int i = 0; i < M; i++) {
+        int r, n;
+        char k;
+        cin >> r >> k >> n;
+        order.push(make_pair(r, make_pair(k, n)));
+    }
+    
+    simulation();
+    
+    return 0;
 }
