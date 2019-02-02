1ca8fd5fba80ac2e647de022687e953bd009f6a0
sssukho <sssukho@gmail.com>
2019-02-02 14:16:34 +0900

    백준 2169 로봇 조종하기

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,69 +1,59 @@
 #include <iostream>
-#include <queue>
+#include <algorithm>
+#include <cstring> //memset
 using namespace std;
 
-#define MAX 1000
+const long long MININF = -2e9;
+const int MAX = 1000;
 
-int dy[4] = {1, -1, 0, 0};
-int dx[4] = {0, 0, 1, -1};
+int N, M; //세로 가로
+int Mars[MAX][MAX];
+bool visited[MAX][MAX];
+long long cache[3][MAX][MAX]; //왼쪽 오른쪽 아래
 
-int N, M;
-int graph[MAX][MAX];
-int cache[MAX][MAX][2]; //마지막 2는 벽 뚫기 여부
-
-void bfs() {
-    queue<pair<pair<int,int>, bool>> q; // (y,x), 벽뚫기
-    q.push(make_pair(make_pair(0, 0), 1));
-    cache[0][0][1] = 1;
-
-    while(!q.empty()) {
-        int y = q.front().first.first;
-        int x = q.front().first.second;
-        bool block = q.front().second;
-        q.pop();
-
-        //도착하면
-        if(y == N-1 && x == M-1) {
-            cout << cache[y][x][block] << endl;
-            return;
-        }
-
-        for(int i = 0; i< 4; i++) {
-            int my = y + dy[i];
-            int mx = x + dx[i];
-
-            if(0 <= my && my < N && 0 <= mx && mx < M) {
-                //벽이 있고, 벽을 아직 안 뚫었다면ㄴ
-                if(graph[my][mx] == 1 && block) {
-                    cache[my][mx][0] = cache[y][x][1]+1;
-                    q.push(make_pair(make_pair(my, mx), 0));
-                }
-                //벽이 없고 방문하지 않았던 곳이라면
-                else if(graph[my][mx] == 0 && cache[my][mx][block] == 0) {
-                    cache[my][mx][block] = cache[y][x][block] + 1;
-                    q.push(make_pair(make_pair(my, mx), block));
-                }
-            }
-        }
-    }
-
-    cout << "-1\n";
+long long maxSum(int dir, int y, int x) { //방향, 세로, 가로
+    //목적지 도달시
+    if(y == N-1 && x == M-1)
+        return Mars[y][x];
+    
+    long long &result = cache[dir][y][x];
+    if(result != MININF)
+        return result;
+    
+    visited[y][x] = true; //방문했다고 표시
+    
+    long long temp1 = MININF, temp2 = MININF, temp3 = MININF;
+    
+    //왼쪽
+    if(x - 1 >= 0 && !visited[y][x-1])
+        temp1 = maxSum(0, y, x-1);
+    //오른쪽
+    if(x + 1 < M && !visited[y][x+1])
+        temp2 = maxSum(1, y, x+1);
+    //아래
+    if(y + 1 < N && !visited[y+1][x])
+        temp3 = maxSum(2, y+1, x);
+        
+    visited[y][x] = false; //(y,x) 기준으로 모든 방향을 가봤음에도 최대가 안나오는 경우 고려하여 다시 방문하지 않았다고 표시
+    
+    return result = Mars[y][x] + max(max(temp1, temp2), temp3);
 }
 
 int main() {
     cout << "start" << endl;
     cin.tie(NULL);
     ios::sync_with_stdio("False");
-
+    
     cin >> N >> M;
-
+    
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
-            scanf("%1d", &graph[i][j]);
+            cin >> Mars[i][j];
+            cache[0][i][j] = cache[1][i][j] = cache[2][i][j] = MININF; //초기화, memset 불가능
         }
     }
-
-    bfs();
-
+    
+    memset(visited, false, sizeof(visited));
+    cout << maxSum(0, 0, 0) << endl;
     return 0;
 }
