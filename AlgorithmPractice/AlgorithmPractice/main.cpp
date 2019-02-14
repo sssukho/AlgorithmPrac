263fb3bdbbf21379d6bd970d2457b532704fbad9
sssukho <sssukho@gmail.com>
2019-02-14 22:29:12 +0900

    SWEA 6719 성수의 프로그래밍 강좌 시청

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
-#include <cstring>
+#include <vector>
+#include <cmath>
+#include <algorithm>
 using namespace std;
 
-#define MAX 51
+int T;
+int N, K; //강좌 개수, k개 적절한 순서로 선택
+vector<int> numbers;
 
-int map[MAX][MAX];
-bool visited[MAX][MAX];
-
-int W, H; //너비, 높이 (가로 세로)
-
-int dy[8] = {1, -1, 0, 0, 1, 1, -1, -1};
-int dx[8] = {0, 0, 1, -1, 1, -1, 1, -1};
-
-int cnt = 0;
-
-void dfs(int y, int x) {
-    visited[y][x] = true;
-    
-    for(int i = 0; i < 8; i++) {
-        int my = y + dy[i];
-        int mx = x + dx[i];
-        
-        if(my >= 0 && my < H && mx >= 0 && mx < W) {
-            if(!visited[my][mx] && map[my][mx] == 1) {
-                visited[my][mx] = true;
-                dfs(my,mx);
-            }
-        }
-    }
+bool cmp(int a, int b) {
+    if(a >= b)
+        return true;
+    else
+        return false;
 }
 
 int main() {
@@ -35,33 +20,28 @@ int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
     
-    while(true) {
-        cnt = 0;
-        memset(visited, false, sizeof(visited));
-        memset(map, 0, sizeof(map));
+    cin >> T;
+    
+    for(int t = 0; t < T; t++) {
+        float result = 0.0;
+        numbers.clear();
         
-        cin >> W >> H;
-        if(W == 0 && H == 0)
-            break;
-        
-        //1은 땅, 0은 바다
-        for(int i = 0; i < H; i++) {
-            for(int j = 0; j < W; j++) {
-                cin >> map[i][j];
-            }
+        cin >> N >> K; //N은 강좌 개수, 선택 개수
+        for(int i = 0; i < N; i++) {
+            int input;
+            cin >> input;
+            numbers.push_back(input);
         }
         
-        for(int i = 0; i < H; i++) {
-            for(int j = 0; j < W; j++) {
-                if(!visited[i][j] && map[i][j] == 1) {
-                    dfs(i,j);
-                    cnt++;
-                }
-            }
-        }
+        sort(numbers.begin(), numbers.end(), cmp);
         
-        cout << cnt << endl;
+        for(int i = K-1; i >= 0; i--) {
+            result = (result+numbers[i]) / 2;
+        }
+
+        printf("#%d %.6f\n", t+1, result);
     }
+    
     
     return 0;
 }
