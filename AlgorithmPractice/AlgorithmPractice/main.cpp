41f275118a9c89a97950e2bfc3cb83da0ee83173
sssukho <sssukho@gmail.com>
2019-02-15 18:27:20 +0900

    백준 2468번 안전 영역

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,18 +1,35 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
 #include <algorithm>
+#include <vector>
+#include <cstring>
 using namespace std;
+#define MAX 101
 
-int T;
-int N, K; //강좌 개수, k개 적절한 순서로 선택
-vector<int> numbers;
+int map[MAX][MAX];
+bool visited[MAX][MAX];
 
-bool cmp(int a, int b) {
-    if(a >= b)
-        return true;
-    else
-        return false;
+int N;
+
+int dy[4] = { 1, -1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
+
+int min_height = 100;
+int max_height = 0;
+
+void dfs(int y, int x) {
+    visited[y][x] = true;
+    
+    for(int i = 0; i < 4; i++) {
+        int my = y + dy[i];
+        int mx = x + dx[i];
+        
+        if(my >= 0 && my < N && mx >= 0 && mx < N) {
+            if(!visited[my][mx] && map[my][mx] > min_height) {
+                visited[my][mx] = true;
+                dfs(my, mx);
+            }
+        }
+    }
 }
 
 int main() {
@@ -20,28 +37,36 @@ int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
     
-    cin >> T;
-    
-    for(int t = 0; t < T; t++) {
-        float result = 0.0;
-        numbers.clear();
+    cin >> N;
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            cin >> map[i][j];
+            if(map[i][j] < min_height)
+                min_height = map[i][j];
+            if(map[i][j] > max_height)
+                max_height = map[i][j];
+        }
+    }
+    int ans = 1;
+    while(min_height < max_height) {
+        int cnt = 0;
+        memset(visited, false, sizeof(visited));
         
-        cin >> N >> K; //N은 강좌 개수, 선택 개수
-        for(int i = 0; i < N; i++) {
-            int input;
-            cin >> input;
-            numbers.push_back(input);
+        for(int i = 0 ; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                if(min_height < map[i][j] && !visited[i][j]) {
+                    dfs(i, j);
+                    cnt++;
+                }
+            }
         }
         
-        sort(numbers.begin(), numbers.end(), cmp);
-        
-        for(int i = K-1; i >= 0; i--) {
-            result = (result+numbers[i]) / 2;
-        }
-
-        printf("#%d %.6f\n", t+1, result);
+        ans = max(ans,cnt);
+        min_height++;
     }
     
+    
+    cout << ans << endl;
     
     return 0;
 }
