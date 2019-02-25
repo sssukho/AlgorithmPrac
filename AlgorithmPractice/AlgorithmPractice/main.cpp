e42130617a792d80e151a67e20c42af0c7da9df8
sssukho <sssukho@gmail.com>
2019-02-25 12:17:55 +0900

    백준 1325번 효율적인 해킹

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,67 +1,103 @@
+//#include <iostream>
+//#include <string>
+//#include <algorithm>
+//using namespace std;
+//
+//int n1, n2;
+//string g1, g2;
+//int T;
+//
+//int main() {
+//    cout << "start" << endl;
+//    cin.tie(0);
+//    ios::sync_with_stdio(0);
+//
+//    cin >> n1 >> n2 >> g1 >> g2 >> T;
+//    reverse(g1.begin(), g1.end());
+//    string result = g1 + g2;
+//
+//    int g1size = (int)g1.size();
+//    int g2size = (int)g2.size();
+//
+//    cout << result << endl;
+//
+//    for(int t = 1; t <= T; t++) {
+//        for(int i = g1size-t; i <= g1size-1+t; i++) {
+//            if(g1.find(result[i]) < g1size && g2.find(result[i+1]) < g2size) {
+//                swap(result[i], result[i+1]);
+//            }
+//        }
+//    }
+//
+//
+//
+//    cout << "------------" << endl;
+//    cout << result << endl;
+//
+//
+//
+//    return 0;
+//}
+
 #include <iostream>
-#include <string>
 #include <vector>
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
-//N에서 자기 자신을 찾으면서 나타나는 부분 일치를 이용해 pi[]를 계산한다.
-//pi[i] = N[..i]의 접미사도 되고 접두사도 되는 문자열의 최대 길이
-vector<int> getPartialMatch(const string& N) {
-    int m = (int)N.size();
-    vector<int> pi(m, 0);
-    //KMP로 자기 자신을 찾는다.
-    //N을 N에서 찾는다. begin = 0이면 자기 자신을 찾아버리니까 안됨!
-    int begin = 1, matched = 0;
-    //비교할 문자가 N의 끝에 도달할 때까지 찾으면서 부분 일치를 모두 기록한다.
-    while(begin + matched < m) {
-        if(N[begin + matched] == N[matched]) {
-            ++matched;
-            pi[begin + matched-1] = matched;
-        }
-        else {
-            if(matched == 0)
-                ++begin;
-            else {
-                begin = begin + (matched - pi[matched-1]);
-                matched = pi[matched-1];
-            }
+#define MAX 10001
+
+int N, M;
+vector<int> edge[MAX];
+bool visited[MAX];
+int cnt;
+
+void dfs(int nodeNum) {
+    visited[nodeNum] = true;
+    
+    for(int i = 0; i < edge[nodeNum].size(); i++) {
+        int next = edge[nodeNum][i];
+        
+        if(!visited[next]) {
+            cnt++;
+            dfs(next);
         }
     }
-    return pi;
 }
-
-vector<int> kmpSearch(const string& H, const string& N) {
-    int n = (int)H.size(), m = (int)N.size();
-    vector<int> ret;
-    //pi[i] = N[..i]의 접미사도 되고 접두사도 되는 문자열의 최대 길이
-    vector<int> pi = getPartialMatch(N);
-    //begin = matched = 0 에서부터 시작하자.
-    int begin = 0, matched = 0;
-    
-    while(begin <= n - m) {
-        //만약 H의 해당글자가 N의 해당 글자와 같다면
-        if(matched < m && H[begin+matched] == N[matched]) {
-            ++matched;
-            //결과적으로 m글자가 모두 일치했다면 답에 추가한다.
-            if(matched == m) ret.push_back(begin);
-        }
-        else {
-            //예외 : matched가 0인 경우에서는 다음 칸에서부터 계속
-            if(matched == 0)
-                ++begin;
-            else {
-                begin = begin + (matched - pi[matched-1]);
-                //begin을 옮겼다고 처음부터 다시 비교할 필요가 없다.
-                //옮긴 후에도 pi[matched-1]만큼은 항상 일치하기 때문이다.
-                matched = pi[matched-1];
-            }
-        }
-    }
-    
-    return ret;
-}
-
 
 int main() {
+    cout << "start" << endl;
+    cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin >> N >> M;
     
+    for(int i = 0; i < M; i++) {
+        int a, b;
+        cin >> a >> b;
+        edge[b].push_back(a);
+    }
+    
+    int nodeCnt = 0;
+    vector<int> result;
+    
+    for(int i = 1; i <= N; i++) {
+        memset(visited, false, sizeof(visited));
+        cnt = 0;
+        
+        dfs(i);
+        
+        if(nodeCnt == cnt)
+            result.push_back(i);
+        if(nodeCnt < cnt) {
+            nodeCnt = cnt;
+            result.clear();
+            result.push_back(i);
+        }
+    }
+    
+    sort(result.begin(), result.end());
+    for(int i = 0; i < result.size(); i++)
+        cout << result[i] << " ";
+    cout << endl;
     return 0;
 }
