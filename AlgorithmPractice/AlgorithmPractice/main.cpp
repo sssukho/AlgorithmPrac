5bf2de39b4afffec649bdb43bfd36a7f10b9395d
sssukho <sssukho@gmail.com>
2019-02-27 13:43:42 +0900

    백준 2638번 치즈

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,66 +1,43 @@
-//#include <iostream>
-//#include <string>
-//#include <algorithm>
-//using namespace std;
-//
-//int n1, n2;
-//string g1, g2;
-//int T;
-//
-//int main() {
-//    cout << "start" << endl;
-//    cin.tie(0);
-//    ios::sync_with_stdio(0);
-//
-//    cin >> n1 >> n2 >> g1 >> g2 >> T;
-//    reverse(g1.begin(), g1.end());
-//    string result = g1 + g2;
-//
-//    int g1size = (int)g1.size();
-//    int g2size = (int)g2.size();
-//
-//    cout << result << endl;
-//
-//    for(int t = 1; t <= T; t++) {
-//        for(int i = g1size-t; i <= g1size-1+t; i++) {
-//            if(g1.find(result[i]) < g1size && g2.find(result[i+1]) < g2size) {
-//                swap(result[i], result[i+1]);
-//            }
-//        }
-//    }
-//
-//
-//
-//    cout << "------------" << endl;
-//    cout << result << endl;
-//
-//
-//
-//    return 0;
-//}
-
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
 using namespace std;
-
-#define MAX 10001
+#define MAX 101
 
 int N, M;
-vector<int> edge[MAX];
-bool visited[MAX];
-int cnt;
+int map[MAX][MAX];
+int dy[4] = { 1, -1, 0, 0};
+int dx[4] = { 0, 0, 1, -1};
 
-void dfs(int nodeNum) {
-    visited[nodeNum] = true;
+//치즈 개수 파악
+int allMelted() {
+    int cnt = 0;
     
-    for(int i = 0; i < edge[nodeNum].size(); i++) {
-        int next = edge[nodeNum][i];
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            if(map[i][j] == 1 || map[i][j] == 2) {
+                //치즈인데 외부 공기에 2면 이상 닿지 않은 치즈들
+                map[i][j] = 1;
+                cnt++;
+            }
+            else map[i][j] = 0;
+        }
+    }
+    
+    return cnt;
+}
+
+//외부 공기로만 계속 들어감
+void dfs(int y, int x) {
+    map[y][x] = -1;
+    
+    for(int i = 0; i < 4; i++) {
+        int my = y + dy[i];
+        int mx = x + dx[i];
         
-        if(!visited[next]) {
-            cnt++;
-            dfs(next);
+        if(my >= 0 && my < N && mx >= 0 && mx < M) {
+            if(map[my][mx] > 0)
+                map[my][mx]++;
+            else if(map[my][mx] == 0)
+                dfs(my, mx);
         }
     }
 }
@@ -69,35 +46,28 @@ int main() {
     cout << "start" << endl;
     cin.tie(0);
     ios::sync_with_stdio(0);
+    
+    int time = 0;
+    
     cin >> N >> M;
     
-    for(int i = 0; i < M; i++) {
-        int a, b;
-        cin >> a >> b;
-        edge[b].push_back(a);
-    }
-    
-    int nodeCnt = 0;
-    vector<int> result;
-    
-    for(int i = 1; i <= N; i++) {
-        memset(visited, false, sizeof(visited));
-        cnt = 0;
-        
-        dfs(i);
-        
-        if(nodeCnt == cnt)
-            result.push_back(i);
-        if(nodeCnt < cnt) {
-            nodeCnt = cnt;
-            result.clear();
-            result.push_back(i);
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            cin >> map[i][j];
         }
     }
     
-    sort(result.begin(), result.end());
-    for(int i = 0; i < result.size(); i++)
-        cout << result[i] << " ";
-    cout << endl;
+    while(true) {
+        int toMelt = allMelted();
+        if(toMelt == 0)
+            break;
+        
+        for(int i = 0; i < toMelt; i++) {
+            dfs(0, 0);
+        }
+        
+        time++;
+    }
+    cout << time << endl;
     return 0;
 }
