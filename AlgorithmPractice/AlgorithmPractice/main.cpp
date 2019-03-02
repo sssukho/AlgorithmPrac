4f1b5c11b18dfe7761198c26d689d45c43ab402c
sssukho <sssukho@gmail.com>
2019-03-02 15:45:32 +0900

    [갱남 알고리즘 스터디] 균형 잡힌 이진 검색 트리 '트립' 구현

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,45 +1,112 @@
+////균형 잡힌 이진 검색 트리 '트립' 구현/////
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <utility>
 using namespace std;
 
-long long T[1000001];
+/************* 트립의 노드를 표현하는 객체의 구현 ****************/
 
-int main() {
-    cout << "start" << endl;
-    cin.tie(0);
-    ios::sync_with_stdio(0);
-
-    int N, M; // 입국심사대, 상근이의 친구들
-    long long max = 0;
-    cin >> N >> M;
-
-    for(int i = 0; i < N; i++) {
-        cin >> T[i];
-        if(max < T[i])
-            max = T[i];
+typedef int KeyType;
+//트립의 한 노드를 저장한다.
+struct Node {
+    //노드에 저장된 원소
+    KeyType key;
+    //이 노드의 우선순위(priority)
+    //이 노드를 루트로 하는 서브트리의 크기(size)
+    int priority, size;
+    //두 자식 노드의 포인터
+    Node *left, *right;
+    //생성자에서 난수 우선순위를 생성하고, size와 left/right를 초기화한다.
+    Node(const KeyType& _key) : key(_key), priority(rand()),
+        size(1), left(NULL), right(NULL) {
+        
     }
-
-    //가장 오래 걸리는 시간이 가장 시간이 오래걸리는 심사대에서 모든 사람들이 심사를 받는 경우이기 때문.
-    long long left = 1, right = max * M;
-    long long result = max * M;
-
-    while (left <= right) {
-        long long mid = (left + right) / 2;
-        long long total = 0;
-        for(int i = 0; i < N; i++) {
-            total += mid / T[i];
-        }
-
-        if(total < M) {
-            left = mid + 1;
-        }
-        else {
-            if(result > mid)
-                result = mid;
-            right = mid - 1;
-        }
+    
+    void setLeft(Node* newLeft) {
+        left = newLeft;
+        calcSize();
     }
-    cout << result << endl;
-    return 0;
+    
+    void setRight(Node* newRight) {
+        right = newRight;
+        calcSize();
+    }
+    //size 멤버를 갱신한다.
+    void calcSize() {
+        size = 1;
+        if(left)
+            size = size + left->size;
+        if(right)
+            size = size + right->size;
+    }
+};
+
+/************* 트립에서의 노드 추가와 트립 쪼개기 구현 ****************/
+
+typedef pair<Node*, Node*> NodePair;
+//root를 루트로 하는 트립을 key 미만의 값과 이상의 값을 갖는 두개의 트립으로 분리
+NodePair split(Node* root, KeyType key) {
+    if(root == NULL)
+        return NodePair(NULL, NULL);
+    //루트가 key 미만이면 오른쪽 서브트리를 쪼갠다.
+    if(root->key < key) {
+        NodePair rs = split(root->right, key);
+        root->setRight(rs.first);
+        return NodePair(root, rs.second);
+    }
+    //루트가 key 이상이면 왼쪽 서브트리를 쪼갠다.
+    NodePair ls = split(root->left, key);
+    root->setLeft(ls.second);
+    return NodePair(ls.first, root);
+}
+
+//root를 루트로 하는 트립에 새 노드 node를 삽입한 뒤 결과 트립의 루트를 반환한다.
+Node* insert(Node* root, Node* node) {
+    if(root == NULL)
+        return node;
+    //node가 루트를 대체해야 한다. 해당 서브트리를 반으로 잘라 각각 자손으로 한다.
+    if(root->priority < node->priority) {
+        NodePair splitted = split(root, node->key);
+        node->setLeft(splitted.first);
+        node->setRight(splitted.second);
+        return node;
+    }
+    else if(node->key < root->key)
+        root->setLeft(insert(root->left, node));
+    else
+        root->setRight(insert(root->right, node));
+    
+    return root;
+}
+
+/************* 트립에서 노드의 삭제와 합치기 연산의 구현 ****************/
+
+//a와 b가 두 개의 트립이고, max(a) < min(b)일 때 이 둘을 합친다.
+Node* merge(Node* a, Node* b) {
+    if(a == NULL)
+        return b;
+    if(b == NULL)
+        return a;
+    if(a->priority < b->priority) {
+        b->setLeft(merge(a, b->left));
+        return b;
+    }
+    a->setRight(merge(a->right, b));
+    return a;
+}
+
+//root를 루트로 하는 트립에서 key를 지우고 그 결과 트립의 루트를 반환한다.
+Node* erase(Node* root, KeyType key) {
+    if(root == NULL)
+        return root;
+    //root를 지우고 양 서브트리를 합친 뒤 반환한다.
+    if(root->key == key) {
+        Node* ret = merge(root->left, root->right);
+        delete root;
+        return ret;
+    }
+    if(key < root->key)
+        root->setLeft(erase(root->left, key));
+    else
+        root->setRight(erase(root->right, key));
+    return root;
 }
