d7ed18cd6b3dce302cdda54ece5a39c4bd5b3d74
sssukho <sssukho@gmail.com>
2019-03-05 21:18:32 +0900

    백준 2533번 사회망 서비스(SNS)

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,40 +1,53 @@
 #include <iostream>
-#include <cstring>
 #include <vector>
-#include <utility>
-#include <memory.h>
+#include <cstring>
 #include <algorithm>
 using namespace std;
-#define MAX 9
+#define MAX 1000001
 
-int N, M; //세로 가로
-int map[MAX][MAX];
-int copyMap[MAX][MAX];
-vector<pair<int, int>> virus; //y, x
-vector<pair<int, int>> empty; //y, x
-int emptySize = 0;
-int emptyCnt;
-int ans = 0;
+int N;
+vector<int> friends[MAX]; // 처음 입력하는 친구 관계
+vector<int> dirNode[MAX]; // 단방향 그래프 저장
+bool visited[MAX];
+int cache[MAX][2]; //노드, earlyAdopter?
 
-vector<int> comb;
-
-int dy[4] = {1, -1, 0, 0};
-int dx[4] = {0, 0, 1, -1};
-
-//바이러스 퍼지게 하는 dfs
-void dfs(int y, int x) {
-    copyMap[y][x] = 2;
+void dfs(int node) {
+    visited[node] = true;
     
-    for(int i = 0; i < 4; i++) {
-        int my = y + dy[i];
-        int mx = x + dx[i];
+    for(int i = 0; i < (int)friends[node].size(); i++) {
+        int next = friends[node][i];
         
-        if(my >= 0 && my < N && mx >= 0 && mx < M) {
-            if(copyMap[my][mx] == 0) {
-                dfs(my, mx);
-            }
+        if(!visited[next]) {
+            dirNode[node].push_back(next);
+            dfs(next);
         }
     }
+}
+
+int getEarlyAdopter(int node, bool isEarlyAdopter) {
+    int &result = cache[node][isEarlyAdopter];
+    if(result != -1)
+        return result;
+    
+    //본인이 얼리어댑터이면 자식은 어떻든 상관 없다.
+    if(isEarlyAdopter) {
+        result = 1; //얼리 어댑터이므로
+        for(int i = 0; i < (int)dirNode[node].size(); i++) {
+            int next = dirNode[node][i];
+            result += min(getEarlyAdopter(next, true), getEarlyAdopter(next, false));
+        }
+    }
+    
+    //본인이 얼리어댑터가 아니면 자식은 무조건 얼리어댑터여야 한다.
+    else {
+        result = 0; //얼리어댑터가 아니므로
+        for(int i = 0; i < (int)dirNode[node].size(); i++) {
+            int next = dirNode[node][i];
+            result += getEarlyAdopter(next, true);
+        }
+    }
+    
+    return result;
 }
 
 int main() {
@@ -42,65 +55,25 @@ int main() {
     cin.tie(0);
     ios::sync_with_stdio(0);
     
-    cin >> N >> M;
+    cin >> N;
     
-    //0은 빈칸, 1은 벽, 2는 바이러스
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < M; j++) {
-            cin >> map[i][j];
-            if(map[i][j] == 2)
-                virus.push_back(make_pair(i, j));
-            
-            if(map[i][j] == 0) {
-                emptySize++;
-                empty.push_back(make_pair(i, j));
-            }
-            
-        }
+    for(int i = 0; i < N - 1; i++) {
+        int node1, node2;
+        cin >> node1 >> node2;
+        
+        friends[node1].push_back(node2);
+        friends[node2].push_back(node1);
     }
     
-    for(int i = 0; i < 3; i++)
-        comb.push_back(1);
+    memset(cache, -1, sizeof(cache));
     
-    for(int i = 0; i < emptySize-3; i++)
-        comb.push_back(0);
+    dfs(1); //1이 루트
     
-    sort(comb.begin(), comb.end());
-    
-    do {
-        emptyCnt = 0;
-        memcpy(copyMap, map, sizeof(map));
-        
-        //벽 세워보기
-        for(int i = 0 ; i < emptySize; i++) {
-            if(comb[i] == 1) {
-                int y = empty[i].first;
-                int x = empty[i].second;
-                copyMap[y][x] = 1;
-            }
-        }
-        
-        //바이러스 퍼지게
-        for(int i = 0; i < (int)virus.size(); i++) {
-            int y = virus[i].first;
-            int x = virus[i].second;
-            
-            dfs(y, x);
-        }
-        
-        for(int i = 0; i < N; i++)
-            for(int j = 0; j < M; j++)
-                if(copyMap[i][j] == 0)
-                    emptyCnt++;
-        
-        ans = max(ans, emptyCnt);
-        
-    }while(next_permutation(comb.begin(), comb.end()));
-    
-    cout << ans << endl;
+    //루트가 얼리어댑터일 수도 아닐 수도 있으므로
+    cout << min(getEarlyAdopter(1, false), getEarlyAdopter(1, true)) << endl;
     
     return 0;
 }
 
-//벽을 다 세워봐야함
-//0인 벽의 카운ㄴㅌ
+//자신이 얼리어답터인 경우 -> 자식들은 얼리 어답터가 아니어도 된다.
+//자신이 얼리어답터가 아닌 경우 -> 이웃한 모두가 얼리어답터여야 한다.
