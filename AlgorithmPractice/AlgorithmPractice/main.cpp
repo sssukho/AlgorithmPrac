35484cbfb82c278f765dc777cad976d9f1edde78
sssukho <sssukho@gmail.com>
2019-03-07 18:54:56 +0900

    백준 14501번 퇴사 삼성 기출

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,83 +1,48 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
-#define MAX 51
+#define MAX 16
 
-int N, M;
-int map[MAX][MAX];
-int y, x, dir;
-int ans;
-
-int dy[4] = {-1, 0, 1, 0};
-int dx[4] = {0, 1, 0, -1};
-
-int getBackDir(int dir) {
-    if(dir == 0)
-        return 2;
-    else if(dir == 1)
-        return 3;
-    else if(dir == 2)
-        return 0;
-    else
-        return 1;
-}
-
-int getDir(int dir) {
-    if(dir == 0)
-        return 3;
-    else if(dir == 1)
-        return 0;
-    else if(dir == 2)
-        return 1;
-    else
-        return 2;
-}
-
+int N; //상담할 수 있는 기간ㄴ
+int dp[MAX];
+int t[MAX];
+int p[MAX];
 
 int main() {
     cout << "start" << endl;
     cin.tie(0);
     ios::sync_with_stdio(0);
     
-    cin >> N >> M;
-    cin >> y >> x >> dir;
+    cin >> N;
     
-    for(int i = 0; i < N; i++)
-        for(int j = 0; j < M; j++)
-            cin >> map[i][j];
+    for(int i = 1; i <= N; i++) {
+        int T, P;
+        cin >> T >> P;
+        t[i] = T;
+        p[i] = P;
+        dp[i] = p[i];
+    }
     
-    while(true) {
-        //현재 위치 청소
-        if(map[y][x] == 0) {
-            map[y][x] = 2;
-            ans++;
-        }
-        
-        //큰 2번으로 다시 돌아가는 flag
-        int check = 0;
-        for(int i = 0; i < 4; i++) { //2.2
-            dir = getDir(dir);
-            int my = y + dy[dir];
-            int mx = x + dx[dir];
-            
-            if(map[my][mx] == 0) { //2.1
-                y = my;
-                x = mx;
-                check = 1; //2번 돌아가는 flag
-                break;
+    //dp[n] = n일때까지 얻은 수익
+    for(int i = 2; i <= N; i++) {
+        for(int j = 1; j < i; j++) {
+            if(i-j >= t[j]) {
+                dp[i] = max(p[i]+dp[j], dp[i]);
             }
         }
-        if(check == 1)
-            continue;
-        //2.3 후진하기
-        int backDir = getBackDir(dir);
-        
-        y = y + dy[backDir];
-        x = x + dx[backDir];
-        
-        //2.4 끝
-        if(map[y][x] == 1)
-            break;
+    }
+    
+    int ans = 0;
+    
+    for(int i = 1; i <= N; i++) {
+        if(i + t[i] <= N + 1) {
+            if(ans < dp[i]) {
+                ans = dp[i];
+            }
+        }
     }
     
     cout << ans << endl;
+    
+    return 0;
 }
