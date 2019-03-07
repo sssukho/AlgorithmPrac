e91f08a7314b6e452cf5e56fc9ce84f52acb1289
sssukho <sssukho@gmail.com>
2019-03-07 22:06:58 +0900

    백준 6603번 로또

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,48 +1,62 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
-#define MAX 16
 
-int N; //상담할 수 있는 기간ㄴ
-int dp[MAX];
-int t[MAX];
-int p[MAX];
+vector<int> s;
+vector<int> comb;
+vector<int> idx;
+vector<int> output;
+
+int t, k;
 
 int main() {
     cout << "start" << endl;
     cin.tie(0);
     ios::sync_with_stdio(0);
     
-    cin >> N;
-    
-    for(int i = 1; i <= N; i++) {
-        int T, P;
-        cin >> T >> P;
-        t[i] = T;
-        p[i] = P;
-        dp[i] = p[i];
-    }
-    
-    //dp[n] = n일때까지 얻은 수익
-    for(int i = 2; i <= N; i++) {
-        for(int j = 1; j < i; j++) {
-            if(i-j >= t[j]) {
-                dp[i] = max(p[i]+dp[j], dp[i]);
-            }
+    while(true) {
+        s.clear();
+        comb.clear();
+        idx.clear();
+        
+        cin >> k;
+        if(k == 0)
+            break;
+        
+        for(int i = 0; i < k; i++) {
+            int input;
+            cin >> input;
+            s.push_back(input);
         }
-    }
-    
-    int ans = 0;
-    
-    for(int i = 1; i <= N; i++) {
-        if(i + t[i] <= N + 1) {
-            if(ans < dp[i]) {
-                ans = dp[i];
+        
+        for(int i = 0; i < 6; i++)
+            comb.push_back(1);
+        
+        for(int i = 0; i < (int)s.size() - 6; i++)
+            comb.push_back(0);
+        
+        do {
+            idx.clear();
+            for(int i = 0; i < (int)s.size(); i++) {
+                if(comb[i] == 1) {
+                    idx.push_back(i);
+                }
             }
-        }
+            
+            for(int i = 0; i < (int)idx.size(); i++)
+                output.push_back(s[idx[i]]);
+            
+            sort(output.begin(), output.end());
+            
+            for(int i = 0; i < (int)output.size(); i++) {
+                cout << output[i] << ' ';
+            }
+            output.clear();
+            cout << "\n";
+        }while(prev_permutation(comb.begin(), comb.end()));
+        cout << "\n";
     }
-    
-    cout << ans << endl;
     
     return 0;
 }
