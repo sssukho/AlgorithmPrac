29fd06562b87091f3b7c61bc878b11bd660e3c96
sssukho <sssukho@gmail.com>
2019-03-29 18:26:10 +0900

    백준 14499번 주사위 굴리기

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,34 +1,121 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-#define MAX 101
+#define MAX 21
 
-int K; //폭탄 가지고 있는 사람의 번호(~8)
-int N; //질문 개수
-int T; //i번째 질문을 대답하기까지 걸린 시간(단위:초), 그 플레이어의 대답 Z(T,N,P중 하나)
-char Z;
-//3분 30초 = 210초
-int main() {
-    cout << "start" << endl;
-    cin >> K; //폭탄 들고 있는 사람 번호
-    cin >> N; //질문 개수
+struct Dice {
+    int up;
+    int down;
+    int right;
+    int left;
+    int front;
+    int rear;
+    int y;
+    int x;
+};
+
+int map[MAX][MAX];
+int dy[4] = {0, 0, -1, 1}; //1동쪽, 2서쪽, 3북, 4남
+int dx[4] = {1, -1, 0, 0};
+
+int N, M; //세로, 가로
+int X, Y, K;
+vector<int> order;
+
+Dice dice;
+
+void setDice(int dir) {
+    int up = dice.up;
+    int down = dice.down;
+    int right = dice.right;
+    int left = dice.left;
+    int front = dice.front;
+    int rear = dice.rear;
+    //동쪽으로 굴리면
+    if(dir == 0) {
+        dice.down = right;
+        dice.up = left;
+        dice.right = up;
+        dice.left = down;
+        dice.front = front;
+        dice.rear = rear;
+    }
+    //왼쪽으로 굴리면
+    else if(dir == 1) {
+        dice.down = left;
+        dice.up = right;
+        dice.right = down;
+        dice.left = up;
+        dice.front = front;
+        dice.rear = rear;
+    }
+    //북쪽으로 굴리면
+    else if(dir == 2) {
+        dice.down = rear;
+        dice.up = front;
+        dice.right = right;
+        dice.left = left;
+        dice.front = down;
+        dice.rear = up;
+    }
+    //남쪽으로 굴리면
+    else {
+        dice.down = front;
+        dice.up = rear;
+        dice.right = right;
+        dice.left = left;
+        dice.front = up;
+        dice.rear = down;
+    }
+}
+
+void simulation() {
+    dice = { 0, 0, 0, 0, 0, 0, Y, X };
     
-    int sec = 0;
-    while(true) {
-        cin >> T >> Z;
-        sec = sec + T;
+    for(int k = 0; k < K; k++) {
+        int dir = order[k]-1;
+        int my = dice.y + dy[dir];
+        int mx = dice.x + dx[dir];
         
-        if(sec >= 210)
-            break;
+        if(my < 0 || my >= N || mx < 0 || mx >= M)
+            continue;
         
-        if(Z == 'T') {
-            K = (K%8) + 1;
+        if(map[my][mx] == 0) {
+            setDice(dir);
+            map[my][mx] = dice.down;
+            dice.y = my;
+            dice.x = mx;
+            cout << dice.up << endl;
         }
         
-        else
-            continue;
+        else {
+            setDice(dir);
+            dice.down = map[my][mx];
+            map[my][mx] = 0;
+            dice.y = my;
+            dice.x = mx;
+            cout << dice.up << endl;
+        }
+    }
+}
+
+int main() {
+    cin >> N >> M >> Y >> X >> K; //세로, 가로, y(세로), x(가로), 명령 개수
+    
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < M; j++) {
+            cin >> map[i][j];
+        }
+    }
+    map[Y][X] = 0;
+    
+    for(int i = 0; i < K; i++) {
+        int input;
+        cin >> input;
+        order.push_back(input);
     }
     
-    cout << K << endl;
+    simulation();
     
     return 0;
 }
