5c212fe800df0a00c7333c0dca33cf68791c491b
sssukho <sssukho@gmail.com>
2019-04-09 17:47:41 +0900

    백준 14890 경사로

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,149 +1,125 @@
-/*
- 모든 사람마다 계단으로 가는 시간 +1을 벡터에 저장하고 그 백터를 오름차순으로 정렬한다.
- 그 다음 for문 돌면서 계단 내려가는 시간을 더해주는데 만약에 계단에 들억간 사람이 3명 보다 많은 경우엔
- (3 인덱스 전 사람의 총 걸린 시간 - 본인 걸린 시간 + 계단 내려가는 시간)을 더해준다.
- 그렇게 해서 가장 오래 걸린 시간을 ans에 저장
- 그리고 result 최소값 갱신
- */
-
 #include <iostream>
-#include <stdio.h>
-#include <vector>
-#include <algorithm>
+#include <queue>
 using namespace std;
-#define MAX 11
-const int INF = 987654321;
+#define MAX 101
 
-struct pos_people {
+struct pos {
     int y;
     int x;
+    int len;
 };
 
-struct pos_stair {
-    int y;
-    int x;
-    int time;
-};
-
-int T, N;
+int N, L; //size, 경사로 길이
 int map[MAX][MAX];
-vector<pos_people> people;
-vector<pos_stair> stair;
-vector<int> comb;
-vector<int> distance1;
-vector<int> distance2;
+bool visited[MAX][MAX];
+queue<pos> q;
+int ans = 0;
 
-int getTimeToStair(pos_people p, pos_stair s) {
-    int pr = p.y;
-    int pc = p.x;
-    int sr = s.y;
-    int sc = s.x;
+void colCheck() {
+    for(int i = 0; i < N; i++)
+        q.push({0, i, 1});
     
-    return abs(pr-sr) + abs(pc-sc) + 1;
+    while(!q.empty()) {
+        int y = q.front().y;
+        int x = q.front().x;
+        int len = q.front().len;
+        q.pop();
+        
+        if(y+1 >= N){
+            ans++;
+            continue;
+        }
+        
+        if(abs(map[y+1][x]-map[y][x]) >= 2)
+            continue;
+        
+        if(map[y+1][x] == map[y][x])
+            q.push({y+1, x, len+1});
+        
+        //내려갈때
+        if(map[y+1][x] - map[y][x] == -1) {
+            if(y+L >= N)
+                continue;
+            
+            int len_cnt = 0;
+            int diff = map[y+1][x] - map[y][x];
+            for(int i = 1; i <= L; i++) {
+                if(map[y+i][x] - map[y][x] == diff) {
+                    len_cnt++;
+                }
+            }
+            
+            if(len_cnt == L)
+                q.push({y+L, x, 0});
+        }
+        
+        //올라갈때
+        if(map[y+1][x] - map[y][x] == 1) {
+            if(len >= L)
+                q.push({y+1, x, 1});
+        }
+    }
+}
+
+void rowCheck() {
+    for(int i = 0; i < N; i++)
+        q.push({i, 0, 1});
+    
+    while(!q.empty()) {
+        int y = q.front().y;
+        int x = q.front().x;
+        int len = q.front().len;
+        q.pop();
+        
+        if(x+1 >= N){
+            ans++;
+            continue;
+        }
+        
+        if(abs(map[y][x+1]-map[y][x]) >= 2)
+            continue;
+        
+        if(map[y][x+1] == map[y][x])
+            q.push({y, x+1, len+1});
+        
+        //내려갈때
+        if(map[y][x+1] - map[y][x] == -1) {
+            if(x+L >= N)
+                continue;
+            
+            int len_cnt = 0;
+            int diff = map[y][x+1] - map[y][x];
+            
+            for(int i = 1; i <= L; i++) {
+                if(map[y][x+i] - map[y][x] == diff) {
+                    len_cnt++;
+                }
+            }
+            
+            if(len_cnt == L)
+                q.push({y, x+L, 0});
+        }
+        
+        //올라갈때
+        if(map[y][x+1] - map[y][x] == 1) {
+            if(len >= L)
+                q.push({y, x+1, 1});
+        }
+    }
 }
 
 int main() {
     cout << "start" << endl;
-    cin >> T;
+    cin >> N >> L;
     
-    for(int t = 1; t <= T; t++) {
-        people.clear();
-        stair.clear();
-        
-        cin >> N;
-        
-        int result = INF;
-        
-        for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                cin >> map[i][j];
-                
-                if(map[i][j] == 1) {
-                    people.push_back({i, j});
-                }
-                
-                
-                if(map[i][j] >= 2)
-                    stair.push_back({i, j, map[i][j]});
-            }
+    for(int i = 0; i < N; i++) {
+        for(int j = 0; j < N; j++) {
+            cin >> map[i][j];
         }
-        
-        //한쪽으로 다 쏠린 경우도 생각해야하기 때문에 0부터 모든 사람 ㅇㅇ
-        for(int i = 0; i <= people.size(); i++) {
-            comb.clear();
-            
-            for(int j = 0; j < i; j++) {
-                comb.push_back(1);
-            }
-            
-            for(int j = 0; j < people.size() - i; j++) {
-                comb.push_back(0);
-            }
-            
-            do {
-                distance1.clear();
-                distance2.clear();
-                
-                for(int j = 0; j < people.size(); j++) {
-                    if(comb[j] == 1) {
-                        distance1.push_back(getTimeToStair(people[j], stair[0]));
-                    }
-                    else if(comb[j] == 0) {
-                        distance2.push_back(getTimeToStair(people[j], stair[1]));
-                    }
-                }
-                
-                sort(distance1.begin(), distance1.end());
-                sort(distance2.begin(), distance2.end());
-                
-                int cnt = 0;
-                int ans = 0;
-                
-                for(int j = 0; j < distance1.size(); j++) {
-                    cnt++;
-                    if(cnt <= 3)
-                        distance1[j] += stair[0].time;
-                    
-                    else {
-                        if(distance1[j-3] - distance1[j] > 0) {
-                            distance1[j] += (distance1[j-3]-distance1[j]+stair[0].time);
-                        }
-                        else {
-                            distance1[j] += stair[0].time;
-                        }
-                    }
-                    
-                    ans = max(ans, distance1[j]);
-                }
-                
-                cnt = 0;
-                
-                for(int j = 0; j < distance2.size(); j++) {
-                    cnt++;
-                    
-                    if(cnt <= 3)
-                        distance2[j] += stair[1].time;
-                    
-                    else {
-                        if(distance2[j-3] - distance2[j] > 0) {
-                            distance2[j] += (distance2[j-3]-distance2[j]+stair[1].time);
-                        }
-                        else {
-                            distance2[j] += stair[1].time;
-                        }
-                    }
-                    
-                    ans = max(ans, distance2[j]);
-                }
-                
-                result = min(result, ans);
-            }while(prev_permutation(comb.begin(), comb.end()));
-            
-        }
-        
-        printf("#%d %d\n", t, result);
     }
     
-    
+    colCheck();
+    rowCheck();
+    cout << ans << endl;
     return 0;
 }
