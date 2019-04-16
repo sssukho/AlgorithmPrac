a75e60f4869b5030d7c490b928d349d19982aa8f
sssukho <sssukho@gmail.com>
2019-04-16 23:11:14 +0900

    백준 2583번 영역 구하기

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -4,122 +4,77 @@ using namespace std;
 #define MAX 101
 
 struct pos {
-    int y;
-    int x;
-    int len;
+    int x1, y1, x2, y2;
 };
 
-int N, L; //size, 경사로 길이
-int map[MAX][MAX];
+int dy[4] = { 1, -1, 0, 0 };
+int dx[4] = { 0, 0, 1, -1 };
 bool visited[MAX][MAX];
-queue<pos> q;
-int ans = 0;
+int M, N, K; //세로, 가로, 직사각형 개수
+vector<pos> recPos;
+vector<int> result;
+queue<pair<int, int>> q;
+int cellCount = 0;
+int areaCount = 0;
 
-void colCheck() {
-    for(int i = 0; i < N; i++)
-        q.push({0, i, 1});
+void dfs(int y, int x) {
+    visited[y][x] = true;
     
-    while(!q.empty()) {
-        int y = q.front().y;
-        int x = q.front().x;
-        int len = q.front().len;
-        q.pop();
+    for(int i = 0; i < 4; i++) {
+        int my = y + dy[i];
+        int mx = x + dx[i];
         
-        if(y+1 >= N){
-            ans++;
-            continue;
-        }
-        
-        if(abs(map[y+1][x]-map[y][x]) >= 2)
-            continue;
-        
-        if(map[y+1][x] == map[y][x])
-            q.push({y+1, x, len+1});
-        
-        //내려갈때
-        if(map[y+1][x] - map[y][x] == -1) {
-            if(y+L >= N)
-                continue;
-            
-            int len_cnt = 0;
-            int diff = map[y+1][x] - map[y][x];
-            for(int i = 1; i <= L; i++) {
-                if(map[y+i][x] - map[y][x] == diff) {
-                    len_cnt++;
-                }
+        if(my >=1 && my <= M && mx >= 1 && mx <= N) {
+            if(!visited[my][mx]) {
+                cellCount++;
+                dfs(my, mx);
             }
-            
-            if(len_cnt == L)
-                q.push({y+L, x, 0});
-        }
-        
-        //올라갈때
-        if(map[y+1][x] - map[y][x] == 1) {
-            if(len >= L)
-                q.push({y+1, x, 1});
         }
     }
 }
 
-void rowCheck() {
-    for(int i = 0; i < N; i++)
-        q.push({i, 0, 1});
-    
-    while(!q.empty()) {
-        int y = q.front().y;
-        int x = q.front().x;
-        int len = q.front().len;
-        q.pop();
+void makeArea() {
+    for(int r = 0; r < recPos.size(); r++) {
+        int y1 = recPos[r].y1;
+        int x1 = recPos[r].x1;
+        int y2 = recPos[r].y2;
+        int x2 = recPos[r].x2;
         
-        if(x+1 >= N){
-            ans++;
-            continue;
-        }
-        
-        if(abs(map[y][x+1]-map[y][x]) >= 2)
-            continue;
-        
-        if(map[y][x+1] == map[y][x])
-            q.push({y, x+1, len+1});
-        
-        //내려갈때
-        if(map[y][x+1] - map[y][x] == -1) {
-            if(x+L >= N)
-                continue;
-            
-            int len_cnt = 0;
-            int diff = map[y][x+1] - map[y][x];
-            
-            for(int i = 1; i <= L; i++) {
-                if(map[y][x+i] - map[y][x] == diff) {
-                    len_cnt++;
-                }
-            }
-            
-            if(len_cnt == L)
-                q.push({y, x+L, 0});
-        }
-        
-        //올라갈때
-        if(map[y][x+1] - map[y][x] == 1) {
-            if(len >= L)
-                q.push({y, x+1, 1});
-        }
+        for(int i = y1; i <= y2; i++)
+            for(int j = x1; j <= x2; j++)
+                visited[i][j] = true;
     }
 }
 
 int main() {
     cout << "start" << endl;
-    cin >> N >> L;
+    cin >> M >> N >> K;
+    for(int k = 0; k < K; k++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        recPos.push_back({x1+1, y1+1, x2, y2});
+    }
     
-    for(int i = 0; i < N; i++) {
-        for(int j = 0; j < N; j++) {
-            cin >> map[i][j];
+    makeArea();
+    
+    for(int i = 1; i <= M; i++) {
+        for(int j = 1; j <= N; j++) {
+            if(!visited[i][j]) {
+                cellCount = 1;
+                dfs(i, j);
+                areaCount++;
+                result.push_back(cellCount);
+            }
         }
     }
     
-    colCheck();
-    rowCheck();
-    cout << ans << endl;
+    
+    sort(result.begin(), result.end());
+    
+    cout << areaCount << endl;
+    for(int i = 0; i < result.size(); i++) {
+        cout << result[i] << ' ';
+    }
+    cout << endl;
     return 0;
 }
