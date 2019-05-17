1d84f3d9340c744b2782107b47d425a5ab880bf5
sssukho <sssukho@gmail.com>
2019-05-17 16:14:41 +0900

    프로그래머스 42587 프린터

--- AlgorithmPractice/AlgorithmPractice/main.cpp
+++ AlgorithmPractice/AlgorithmPractice/main.cpp
@@ -1,86 +1,62 @@
+/*
+ 1. 인쇄 대기목록의 가장 앞에 있는 문서(J)를 대기목록에서 꺼냅니다.
+ 2. 나머지 인쇄 대기목록에서 J보다 중요도가 높은 문서가 한 개라도 존재하면 J를 대기목록의 가장 마지막에 넣습니다.
+ 3. 그렇지 않으면 J를 인쇄합니다.
+ */
+//a, b, c, d
+//2, 1, 3, 2
+
 #include <iostream>
-#include <vector>
 #include <string>
-#include <stack>
+#include <vector>
+#include <queue>
 #include <utility>
+#include <algorithm>
 
 using namespace std;
 
-vector<int> solution(vector<int> heights) {
-    vector<int> answer;
-    stack<pair<int, int>> st;
-    stack<int> output;
+int solution(vector<int> priorities, int location) {
+    int answer = 0;
+    int count = 0;
+    queue<pair<int, int>> q;
+    priority_queue<int> pq;
     
-    for(int i = 0; i < (int)heights.size(); i++) {
-        st.push(make_pair(heights[i], i));
+    for(int i = 0; i < (int)priorities.size(); i++) {
+        //처음 대기목록 순서, 중요도
+        q.push(make_pair(i, priorities[i]));
+        //우선순위 큐에 작업 중요도
+        pq.push(priorities[i]);
     }
     
-    while(!st.empty()) {
-        int top = st.top().first;
-        int top_idx = st.top().second;
-        
-        for(int i = top_idx; i >= 0; i--) {
-            if(heights[i] > top) {
-                output.push(i+1);
-                break;
+    while(!q.empty()) {
+        //출력순서가 왔을 때
+        if(q.front().second == pq.top()) {
+            //처음 대기 목록 순서와 문제에서 요청된 문서번호 같을 때
+            if(q.front().first == location)
+                return answer + 1;
+            else {
+                //출력 후 작업 큐와 우선순위 큐에서 문서를 하나씩 pop
+                answer++;
+                q.pop();
+                pq.pop();
             }
-            
-            if(i == 0)
-                output.push(0);
         }
-        
-        st.pop();
-    }
-    
-    while(!output.empty()) {
-        answer.push_back(output.top());
-        output.pop();
+        else {
+            //출력순서가 아니므로 front에 있는 값을 큐의 맨뒤로 이동
+            q.push(q.front());
+            q.pop();
+        }
     }
     
     return answer;
 }
 
 int main() {
-    vector<int> h1, h2, h3;
-    //h1 = {6, 9, 5, 7, 4};
-    h1.push_back(6);
-    h1.push_back(9);
-    h1.push_back(5);
-    h1.push_back(7);
-    h1.push_back(4);
-    //h2 = {3, 9, 9, 3, 5, 7, 2};
-    h2.push_back(3);
-    h2.push_back(9);
-    h2.push_back(9);
-    h2.push_back(3);
-    h2.push_back(5);
-    h2.push_back(7);
-    h2.push_back(2);
-    //h3 = {1, 5, 3, 6, 7, 6, 5};
-    h3.push_back(1);
-    h3.push_back(5);
-    h3.push_back(3);
-    h3.push_back(6);
-    h3.push_back(7);
-    h3.push_back(6);
-    h3.push_back(5);
+    vector<int> pri1 = {2, 1, 3, 2};
+    vector<int> pri2 = {1, 1, 9, 1, 1, 1};
     
-    vector<int> ans1, ans2, ans3;
-    ans1 = solution(h1);
-    ans2 = solution(h2);
-    ans3 = solution(h3);
-    
-    for(auto x : ans1)
-        cout << x << ' ';
-    cout << endl;
-    
-    for(auto x : ans2)
-        cout << x << ' ';
-    cout << endl;
-    
-    for(auto x : ans3)
-        cout << x << ' ';
-    cout << endl;
+    cout << solution(pri1, 2) << endl;
+    cout << solution(pri2, 5) << endl;
     
     return 0;
 }
