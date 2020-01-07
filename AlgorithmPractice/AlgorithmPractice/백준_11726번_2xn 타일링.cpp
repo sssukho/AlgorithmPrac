73a7f01f3c15e487f6792f7272c1d6257878c3db
sssukho <sssukho@gmail.com>
2020-01-08 00:42:08 +0900

    백준 11726번 2xn 타일링

--- AlgorithmPractice/AlgorithmPractice/백준_11726번_2xn 타일링.cpp
+++ AlgorithmPractice/AlgorithmPractice/백준_11726번_2xn 타일링.cpp
@@ -1,23 +1,23 @@
 #include <iostream>
 #include <algorithm>
 using namespace std;
-#define MAX 1001
 
-int cache[MAX];
+int cache[1000];
 
 int main() {
     cout << "start" << endl;
     int n;
     cin >> n;
     
-    cache[0] = 1;
     cache[1] = 1;
     cache[2] = 2;
+    cache[3] = 3;
     
-    for(int i = 3; i <= n; i++) {
+    for(int i = 4; i <= n; i++) {
         cache[i] = (cache[i-1] + cache[i-2]) % 10007;
     }
     
     cout << cache[n] << endl;
+    
     return 0;
 }
